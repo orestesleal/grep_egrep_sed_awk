f189e1277177d54059eedeb20e0f33e89fab93b9
Orestes Leal Rodriguez <olealrd1981@gmail.com>
2018-06-24 12:58:16 -0400

    Update to some markups
    support for .console markups for representing console output, added .draft mark to mark text as a draft and that is not going to be included in the final document (will be treated like a comment), added comment trimming, among others

--- format.sh
+++ format.sh
@@ -1,15 +1,24 @@
 #! /bin/sh
 
 # formatter for a kind of markup or simple formatting language
-# I just invented. see doc.txt
+# I just invented which is based on the classic unix tools for
+# formatting and typesetting although 100 times more basic. 
+# see doc.txt for format (not finished)
 # Orestes Leal Rodriguez, 2018
 
 sed '
-s/^\[=\]$/ ======================================================/
-s/\.hd1/ >> /
-s/\.hd[23]/  -> |/
-
+/^#.*$/d
+/^\.draft/,/^\.\\draft/d
+/^\.console/,/^\.\\console/{
+   s/^ /    /
+   /^\.console/d
+   /^\.\\console/d
+}
+s/^\[\(─────\)\]/\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1\1/
+s/^\.CH *//
+s/\.hd[23]/  /
 /^\.text/,/\.\\text/{   
 	/^\.[\]*text/d
-	s/^ /     /
+	s/^ /    /
 }' $*
+
