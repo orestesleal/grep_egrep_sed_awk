3aae7cfaea73e6384011caa6f66bf6f3ebb2c395
Orestes Leal Rodriguez <olealrd1981@gmail.com>
2018-06-28 21:11:43 -0400

    added header with sed(1) + some minor updates
    Added header for oui and vendor to make it look like a table. sed is inserting the text in the output before the first  iteration of the pattern space is in place. Removed some unnecesary comnents to make the code looks cleaner

--- oui.sh
+++ oui.sh
@@ -1,15 +1,13 @@
-#!/bin/sh
+#! /usr/local/bin/ksh93
 
 # Description: Extract Ethernet Mac Addresses from an input file
-#              and look for the vendor in the OUI db. The DB will
-#              be downloaded in case it doesn't exist.
-# Orestes Leal Rodriguez, 2018.
-# TODOs: 1. walk the PATH env variable to find 'curl' & 'wget'
+#              and look for the vendor in the OUI db. 
+# ~ Orestes Leal Rodriguez, 2018.
 
 OUIDB=http://standards-oui.ieee.org/oui.txt
 
 if [ ! -f "oui.txt" ]; then
-  echo "### warning: the OUI db is not here, downloading now..."
+  echo "# info: the OUI db oui.txt is not here, downloading now..."
   which wget 1>/dev/null
   if [ "$?" -eq "0" ]; then
      wget "$OUIDB" 2>/dev/null
@@ -19,20 +17,27 @@ if [ ! -f "oui.txt" ]; then
      if [ "$?" -eq "0" ]; then
          curl $OUIDB
      else
-         echo "### Error: neither CURL or WGET were found, install one of them"
+         echo "# Error: neither CURL or WGET were found, install one of them"
          exit 1
      fi
   fi
 fi
+
 # Speed Hack. I get a 2x improvement in speed filtering the interesting fields
-awk 'gsub(/ *\(hex\).\t/, " ")' oui.txt > /tmp/oui2.txt
-awk '{
+gawk 'gsub(/ *\(hex\).\t/, " ")' oui.txt > /tmp/oui2.txt
+gawk '{
        if ($1 ~ /^ *([[:xdigit:]]{2}[-:.]){5}[[:xdigit:]]{1,2} *$/)
            print toupper($1)
      }' $1 \
  |
 sed 's/[:\.]/-/g' | \
 
-for mac in $(awk -F"-" '{ print $1"-"$2"-"$3 }'); do
-    awk /$mac/ /tmp/oui2.txt
-  done  | sort   # sort by Vendor.
+for mac in $(gawk -F"-" '{ print $1"-"$2"-"$3 }'); do
+    gawk /$mac/ /tmp/oui2.txt
+  done  | sort  | \
+
+sed '1i\
+\
+OUI   -   Vendor\
+===       ======\
+'
