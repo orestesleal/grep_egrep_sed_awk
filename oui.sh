009bee5a3658f70b927f2b5451db5336797d5dce
Orestes Leal Rodriguez <olealrd1981@gmail.com>
2018-06-30 14:50:28 -0400

    error checking added
    Error checking added from the shell side to check if the download was successfull

--- oui.sh
+++ oui.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 
-# Description: Extract Ethernet Mac Addresses from an input file
-#              and look for the vendor in the OUI db. The DB will
-#              be downloaded in case it doesn't exist.
-# Orestes Leal Rodriguez, 2018.
+# ~ Description:
+#      Extract Ethernet Mac Addresses from an input file
+#      and find the vendor for the adapter in the OUI db.
+#   Orestes Leal Rodriguez, 2018.
 
-if [ ! -f oui.txt ]; then
+if [ ! -f oui.txt ];
+then
    echo "## manufacturers OUI db not found, downloading..."
 
    gawk 'BEGIN {
@@ -13,15 +14,23 @@ if [ ! -f oui.txt ]; then
       ieee_server = "/inet/tcp/0/standards-oui.ieee.org/80"
       http_request = "GET /oui.txt HTTP/1.1\r\nHost: standards-oui.ieee.org\r\nConnection: close\r\n\r\n"
 
-      print http_request |& ieee_server
+      print http_request |& ieee_server          # sent request over the gawk socket
 
       while ((ieee_server |& getline) > 0)
-        if ( match($0, "(hex)") != 0) {
+        if ( match($0, "(hex)") != 0) {          # match what I need. optimization
            sub(/ *\(hex\).\t/, " ", $0)
            print $0
         }
+
    }' > oui.txt
+
+   if [ "$?" -ne "0" ];
+   then
+      echo "^^ An error has occurred while downloading the oui database, please try again"
+      exit 1
+   fi
 fi
+
 gawk '{
        if ($1 ~ /^ *([[:xdigit:]]{2}[-:.]){5}[[:xdigit:]]{1,2} *$/)
            print toupper($1)
@@ -37,7 +46,7 @@ sort \
  |  \
 sed '1i\
 \
-   OUI  -   Vendor\
-   ===      ======\
+   OUI   -   Vendor\
+   ===       ======\
 '
 
