8bbef8ca4d295d912fc5dced9cafe562bff122f7
Orestes Leal Rodriguez <olealrd1981@gmail.com>
2018-06-30 14:29:42 -0400

    fixed unwanted "hex" strings on result
    removed an unwanted "hex" string that comes from the original file,  was showing up since I removed the gawk one liner that took care of it, now is integrated fully on the input stream coming from the socket, the filtering take place there and the output saved is as clean as it gets to filtering the macs of the input vs the ouis

--- oui.sh
+++ oui.sh
@@ -1,4 +1,4 @@
-#! /usr/local/bin/ksh93
+#! /bin/sh
 
 # Description: Extract Ethernet Mac Addresses from an input file
 #              and look for the vendor in the OUI db. The DB will
@@ -9,25 +9,26 @@ if [ ! -f oui.txt ]; then
    echo "## manufacturers OUI db not found, downloading..."
 
    gawk 'BEGIN {
-      ieee_server = "/inet/tcp/0/standards-oui.ieee.org/80" 
+
+      ieee_server = "/inet/tcp/0/standards-oui.ieee.org/80"
       http_request = "GET /oui.txt HTTP/1.1\r\nHost: standards-oui.ieee.org\r\nConnection: close\r\n\r\n"
-      
-      print http_request |& ieee_server       # sent the request over the gawk socket
-      
+
+      print http_request |& ieee_server
+
       while ((ieee_server |& getline) > 0)
-        if ( match($0, "(hex)") != 0)         # match what I need, optimization
+        if ( match($0, "(hex)") != 0) {
+           sub(/ *\(hex\).\t/, " ", $0)
            print $0
+        }
    }' > oui.txt
-
 fi
-
 gawk '{
        if ($1 ~ /^ *([[:xdigit:]]{2}[-:.]){5}[[:xdigit:]]{1,2} *$/)
            print toupper($1)
      }' $1 \
  |
 sed 's/[:\.]/-/g' \
- | 
+ |
 for mac in $(gawk -F"-" '{ print $1"-"$2"-"$3 }'); do
     gawk /$mac/ oui.txt
 done \
@@ -36,6 +37,7 @@ sort \
  |  \
 sed '1i\
 \
-OUI   -   Vendor\
-===       ======\
+   OUI  -   Vendor\
+   ===      ======\
 '
+
