e04d167195807ececc3e44fcd63640c6196e7881
Orestes Leal Rodriguez <olealrd1981@gmail.com>
2018-06-30 14:06:46 -0400

    great update, gawk used to download OUI db, etc
    Added a universal downloader that doesn't need curl(1) or wget(1), those shell lines with the logic were removed and replaced by the language level socket implementation of gawk(1). I build an HTTP 1.1, setup and inet file and request and send it to the remote host
    then I get the reply from 'getline'.
    
    figuring out the HTTP1.1 behavior was tricky but after reading the standard, I found that the connection was being keep open (and gawk was left on an process's 'sbwait state'), therefore in the request line I request that the connection should be closed (Connection: close) after the data is transmitted, that way 'getline' will now and the loop will end returning to the next statement(s)
    
    This is a desired update to this utility, more to come..yay...

--- oui.sh
+++ oui.sh
@@ -1,43 +1,39 @@
 #! /usr/local/bin/ksh93
 
 # Description: Extract Ethernet Mac Addresses from an input file
-#              and look for the vendor in the OUI db.
-# ~ Orestes Leal Rodriguez, 2018.
+#              and look for the vendor in the OUI db. The DB will
+#              be downloaded in case it doesn't exist.
+# Orestes Leal Rodriguez, 2018.
 
-OUIDB=http://standards-oui.ieee.org/oui.txt
+if [ ! -f oui.txt ]; then
+   echo "## manufacturers OUI db not found, downloading..."
 
-if [ ! -f "oui.txt" ]; then
-  echo "# info: the OUI db oui.txt is not here, downloading now..."
-  which wget 1>/dev/null
-  if [ "$?" -eq "0" ]; then
-     wget "$OUIDB" 2>/dev/null
-  else
-     # fall back to curl, try to find it...
-     which curl 1>/dev/null
-     if [ "$?" -eq "0" ]; then
-         curl $OUIDB -O
-     else
-         echo "# Error: neither CURL or WGET were found, install one of them"
-         exit 1
-     fi
-  fi
+   gawk 'BEGIN {
+      ieee_server = "/inet/tcp/0/standards-oui.ieee.org/80" 
+      http_request = "GET /oui.txt HTTP/1.1\r\nHost: standards-oui.ieee.org\r\nConnection: close\r\n\r\n"
+      
+      print http_request |& ieee_server       # sent the request over the gawk socket
+      
+      while ((ieee_server |& getline) > 0)
+        if ( match($0, "(hex)") != 0)         # match what I need, optimization
+           print $0
+   }' > oui.txt
+
 fi
 
-# Speed Hack. I get a 2x improvement in speed filtering the interesting fields
-gawk 'gsub(/ *\(hex\).\t/, " ")' oui.txt > /tmp/oui2.txt
 gawk '{
        if ($1 ~ /^ *([[:xdigit:]]{2}[-:.]){5}[[:xdigit:]]{1,2} *$/)
            print toupper($1)
      }' $1 \
  |
 sed 's/[:\.]/-/g' \
- |
+ | 
 for mac in $(gawk -F"-" '{ print $1"-"$2"-"$3 }'); do
-   gawk /$mac/ /tmp/oui2.txt
+    gawk /$mac/ oui.txt
 done \
  |
 sort \
- |
+ |  \
 sed '1i\
 \
 OUI   -   Vendor\
